dfae86ef836c1944bcb91d2648e2e93826dea573
ingsuifon <ingsuifon@vip.qq.com>
2023-10-08 22:32:22 +0800

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -12,6 +12,7 @@ pub fn capitalize_first(input: &str) -> String {
     match c.next() {
         None => String::new(),
         Some(first) => first.to_uppercase().to_string() + c.as_str(),
+        
     }
 }
 
