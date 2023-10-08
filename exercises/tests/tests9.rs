521d68538c11b85ee0ef3dd3d57174cb1e4022dd
ingsuifon <ingsuifon@vip.qq.com>
2023-10-08 22:47:44 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -30,8 +30,10 @@
 // I AM NOT DONE
 
 extern "Rust" {
-    fn my_demo_function(a: u32) -> u32;
-    fn my_demo_function_alias(a: u32) -> u32;
+    #[no_mangle]
+    pub fn my_demo_function(a: u32) -> u32;
+    #[no_mangle]
+    pub fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
